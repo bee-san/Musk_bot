82b9a1c3f36ce2577bba30bf85292dc2cdc1dae0
Brandon <brandonskerritt51@gmail.com>
2018-01-13 19:51:27 +0000

    This is an automated script to quickly update this Git repo. No important features here.

--- main.py
+++ main.py
@@ -4,17 +4,10 @@ from markovbot import MarkovBot
 
 with open("tokens.txt", "r") as f:
     f_read = f.readline()
-    print(f_read)
-    access_token = f_read.split(" ")[2][0:-1]
-    print(access_token)
-    access_token_secret = f_read[1].split(" ")[1]
-    cons_key = f_read[2].split(" ")[1]
-    cons_secret = f_read[3].split(" ")[1]
-
-print(access_token)
-print(access_token_secret)
-print(cons_key)
-print(cons_secret)
+    access_token = "952160751157960704-ek5scwczzdQKgO9MRrDZbAAQV4L3ZAv" # f_read.split(" ")[2][0:-1]
+    access_token_secret = "o3cygwweV3uXvY3MIg1aNGq761gCcX6yUWY4hIZkPvU0h"
+    cons_key = "36wM5DmrtL3Rb0kqbZ3DJkcYD"
+    cons_secret = "a3udTGncmjkf08xH8NnVGQni6CNzp3WpU8yjHuOtFeUcxonpyg"
 
 # Initialise a MarkovBot instance
 tweetbot = MarkovBot()
@@ -25,3 +18,14 @@ dirname = os.path.dirname(os.path.abspath(__file__))
 book = os.path.join(dirname, 'ElonMuskTxt.txt')
 # Make your bot read the book!
 tweetbot.read(book)
+
+my_first_text = tweetbot.generate_text(25, seedword=['space', 'Tesla'])
+print("tweetbot says:")
+print(my_first_text)
+
+tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
+
+from time import sleep
+while True:
+    tweetbot.twitter_tweeting_start(days=0, hours=0, minutes=30, keywords=['SpaceX', 'Tesla', 'Mars', 'Solar', 'Do more', '120 hour work week', 'I am not an alien', 'launch'], prefix=None, suffix="#ElonMusk")
+    sleep(30)
